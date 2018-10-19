a90cdd4abec21ddb9362bcb46f60b140113d4242
Udeji Francis <francis.udejiofficial@gmail.com>
2018-10-19 18:46:59 +0100

    All done!

--- sw.js
+++ sw.js
@@ -1,45 +1,76 @@
-const cacheName = "v1";
+const cacheV1 = "news-app-cache-files";
 const filesToCache = [
-
-	"/index.html",
-	"/manifest.json"
-	// "./css/style.css",
-	// "./js/app.js",
-	// "./images/unbox-icon.png",
-	// "./images/unbox-logo.png",
-	// "./images/image_unavailable.jpeg",
-	// "./images/icons/icon-72x72.png",
-	// "./images/icons/icon-96x96.png",
-	// "./images/icons/icon-128x128.png",
-	// "./images/icons/icon-144x144.png",
-	// "./images/icons/icon-152x152.png",
-	// "./images/icons/icon-192x192.png",
-	// "./images/icons/icon-384x384.png",
-	// "./images/icons/icon-512x512.png",
-	// "./images/cat.jpeg",
-    // "./images/logo.png",
-    // "./images/404.jpg"
+  "/index.html",
+  "/manifest.json",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/icons/icon-72x72.png",
+  "/images/icons/icon-96x96.png",
+  "/images/icons/icon-128x128.png",
+  "/images/icons/icon-144x144.png",
+  "/images/icons/icon-152x152.png",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-384x384.png",
+  "/images/icons/icon-512x512.png",
+  "/images/cat.jpeg",
+  "/images/logo.png",
+  "/images/404.jpg"
 ];
 
 // install event
-self.addEventListener('install', e => {
-    console.log("[sw installed]");
-    e.waitUntil(
-        caches.open(cacheName)
-        .then(cache => {
-            cache.addAll([
-                '/',
-                '/css/style.css'
-            ]);
-            console.log(cache)
-        })
-        .catch(error => console.log({error}))
-    );
+self.addEventListener("install", e => {
+  console.log("[sw installed]");
+  e.waitUntil(
+    caches
+      .open(cacheV1)
+      .then(cache => {
+        console.log("[SW] Caching cachefiles");
+        return cache.addAll(filesToCache);
+      })
+      .catch(error => console.log({ error }))
+  );
 });
-
 
 // activate event
-self.addEventListener('activate', e => {
-    console.log(`[SW]Activating ${cacheName}`)
+self.addEventListener("activate", e => {
+  console.log(`[SW Activated]`);
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheV1 === cacheName) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
 
+// fetch event
+self.addEventListener("fetch", e => {
+  console.log("[SW fetched]");
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      if (response) {
+        console.log("[SW] found in cache", e.request.url);
+        return response;
+      }
+
+      const requestClone = e.request.clone();
+
+      fetch(requestClone)
+        .then(response => {
+          if (!response) {
+            console.log("[SW] no response");
+            return response;
+          }
+
+          const responseClone = response.clone();
+          caches.open(cacheV1).then(cache => {
+            cache.put(e.request, responseClone);
+            return response;
+          });
+        })
+        .catch(error => console.log("[SW] ERROR Fetching ", {error}));
+    })
+  );
+});
