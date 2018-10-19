ecb7270813d57a085acf64cf85b7a83c44c2c956
francisudeji <francis.udejiofficial@gmail.com>
2018-10-19 06:09:00 -0700

    Finished basic UI

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 "use strict"
 
-registerServiceWorker();
+// API stuff
+const apiKey     = "cb31e104b8374a858ef6e2fd9bc6f53d";
+const endpoint   = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+const entryPoint = document.querySelector(".main");
+const loader     = document.querySelector(".loader");
 
+
+// register service worker on page load
+document.addEventListener('DOMContentLoaded', e => {
+    registerServiceWorker();
+    fetchHeadlines();
+})
+
+// register service worker function
 function registerServiceWorker() {
     if('serviceWorker' in navigator) {
         navigator.serviceWorker.register('../sw.js')
@@ -12,4 +24,35 @@ function registerServiceWorker() {
     } else {
         console.log("[SW not supported on your browser]")
     }
+}
+
+// fetch Headlines
+function fetchHeadlines() {
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            const { articles } = data;
+            articles.map(article => {
+                const { title, url, urlToImage } = article;
+                loader.style.display = "none";
+                displayHeadlines(title, urlToImage, url);
+            }).join("");
+        })
+        .catch(error => console.log({error}));
+}
+
+
+// display headlines in the DOM
+function displayHeadlines(title, urlToImage, url) {
+    entryPoint.innerHTML += `
+        <div class="card">
+            <div class="card-image">
+                <img src="${urlToImage}" alt="Picture of cat">
+            </div>
+            <div class="card-body">
+                <h3>${title}</h3>
+                <a href="${url}" target="_blank">Read More</a>
+            </div>
+        </div>
+    `;
 }
